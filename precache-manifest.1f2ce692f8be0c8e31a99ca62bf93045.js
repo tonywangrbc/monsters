63b812c54328e3467281a57aad4ab1f1b8718290
Tony Wang <tonywang@Tonys-MacBook-Pro.local>
2020-09-06 20:48:11 -0400

    Updates

---
+++ precache-manifest.1f2ce692f8be0c8e31a99ca62bf93045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f4bb1d4c9421292879b1d44a94d318",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "b3be1bd117b4c6bbf864",
+    "url": "/monsters/static/css/main.fc1b0864.chunk.css"
+  },
+  {
+    "revision": "69b6d0a5e8111e032d70",
+    "url": "/monsters/static/js/2.a55489bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.a55489bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3be1bd117b4c6bbf864",
+    "url": "/monsters/static/js/main.c389f63f.chunk.js"
+  },
+  {
+    "revision": "03919b83b878cef951f1",
+    "url": "/monsters/static/js/runtime-main.cc0b5fe4.js"
+  }
+]);
